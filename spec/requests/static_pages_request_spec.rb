9ec6871a72ed6923db9a231ec82c5c40581a889a
Naoyuki Ishida <39.ishida@gmail.com>
2021-03-02 00:42:13 -0800

    create StaticPages request spec

---
+++ spec/requests/static_pages_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+
+  describe "GET /" do
+    it "returns http success" do
+      get root_path
+      # `get root_path
+      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status "200"
+    end
+  end
+
+end
