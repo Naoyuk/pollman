7deaeaf1da232bcf2c0105630fc36ea52bec5323
Naoyuki Ishida <39.ishida@gmail.com>
2021-03-02 18:39:20 -0800

    6:39

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe 'StaticPages', type: :request do
   describe 'GET /' do
     it 'returns http success' do
       get root_path
-      # `get root_path
       expect(response).to have_http_status(:success)
       expect(response).to have_http_status '200'
     end
