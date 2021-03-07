cd8b285d210b90f9a180d59ebe0421cada218ca6
Naoyuki Ishida <39.ishida@gmail.com>
2021-03-06 19:10:44 -0800

    create request test and pass them

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,22 +1,55 @@
 class VotesController < ApplicationController
+  before_action :set_vote, only: %i(show edit update destroy)
+
   def index
+    @votes = Vote.all
+    #TODO ユーザーモデルを作成したら、そのユーザーが参加する、またはそのユーザーの作成したVoteのみを降順に取得
   end
 
   def new
+    @vote = Vote.new()
   end
 
   def create
+    @vote = Vote.new(vote_params)
+    if @vote.save
+      redirect_to votes_path, notice: 'Vote was successfully created.'
+    else
+      render :new
+    end
   end
 
   def show
+    render :show
   end
 
   def edit
+    render :edit
   end
 
   def update
+    if @vote.update(vote_params)
+      redirect_to votes_path, notice: 'Vote was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if @vote.destroy
+      redirect_to votes_path, alert: 'Vote was deleted.'
+    else
+      redirect_to votes_path, alert: 'Delete is unsuccessfull.'
+    end
+  end
+
+  private
+
+  def set_vote
+    @vote = Vote.find(params[:id])
+  end
+
+  def vote_params
+    params.require(:vote).permit(:title, :deadline)
   end
 end
