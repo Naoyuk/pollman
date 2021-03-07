5a66f001aec34c8a5db583e445b34bd25cc363a3
Naoyuki Ishida <39.ishida@gmail.com>
2021-03-06 16:31:23 -0800

    create polls controller and related files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'polls/index'
+  get 'polls/new'
+  get 'polls/create'
+  get 'polls/show'
+  get 'polls/edit'
+  get 'polls/update'
+  get 'polls/destroy'
   root 'static_pages#home'
   get  '/help', to: 'static_pages#help'
-  get  '/signup',  to: 'users#new'
+  get  '/signup', to: 'users#new'
 end
